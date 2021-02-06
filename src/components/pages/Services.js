53837d8e644d378e10d014b45a805d49c26ba23b
dhanikprakash <dhanikprakash@gmail.com>
2021-02-06 14:01:16 +0000

    bug fixes

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import '../../App.css';
+import Cards from '../Cards';
+import Footer from '../Footer';
 
 export default function Services() {
-  return <h1 className='services'>SERVICES</h1>;
+  return (<> 
+        <Cards />
+      <Footer />
+      </>);
 }
