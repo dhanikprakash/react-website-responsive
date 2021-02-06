dbc4d9c5ac41612c9af571f830185fc33b5fdac8
dhanikprakash <dhanikprakash@gmail.com>
2021-02-06 23:27:58 +0000

    bug fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
 import Home from './components/pages/Home';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { HashRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
@@ -10,16 +10,19 @@ import SignUp from './components/pages/SignUp';
 function App() {
   return (
     <>
-      <Router>
+      <HashRouter basename={process.env.PUBLIC_URL}>
         <Navbar />
-        <Switch>
-          <Route exact path="/"><Redirect to="/home" /></Route>
-          <Route path='/home' exact component={Home} />
-          <Route path='/services' component={Services} />
-          <Route path='/products' component={Products} />
-          <Route path='/sign-up' component={SignUp} />
-        </Switch>
-      </Router>
+        <Route render={({ location }) => (
+          <Switch location={location}>
+            <Route exact path="/"><Redirect to="/home" /></Route>
+            <Route path='/home' exact component={Home} />
+            <Route path='/services' component={Services} />
+            <Route path='/products' component={Products} />
+            <Route path='/sign-up' component={SignUp} />
+          </Switch>
+        )} />
+      </HashRouter>
+
     </>
   );
 }
